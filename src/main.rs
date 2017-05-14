c69ec8ae7b3b9b0dc6d10a36e2e33875e23dcd18
Tiziano Santoro <tiziano88@gmail.com>
2017-05-14 23:38:40 +0100

    colors

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
-#[macro_use]
-extern crate log;
-extern crate env_logger;
-
 extern crate iron;
 extern crate router;
 extern crate params;
 extern crate palette;
-
-// To run, $ cargo run --example simple
-// To use, go to http://localhost:3000/test and see output "test"
-// Or, go to http://localhost:3000 to see a default "OK"
 
 use iron::{Iron, Request, Response, IronResult, Plugin};
 use iron::status;
@@ -19,62 +11,55 @@ use std::process::Command;
 use palette::named;
 
 fn main() {
-    env_logger::init().unwrap();
-
     println!("starting up");
 
     let mut router = Router::new();
     router.get("/", handler, "handler");
-    router.get("/lights", lights_handler, "lights_handler");
-    router.get("/:query", query_handler, "query_handler");
+    router.get("/lights", get_lights_handler, "lights_handler");
+    router.post("/lights", set_lights_handler, "lights_handler");
 
     Iron::new(router).http("0.0.0.0:8888").unwrap();
+}
 
-    fn handler(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "OK")))
-    }
+fn handler(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "OK")))
+}
 
-    fn query_handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions
-            .get::<Router>()
-            .unwrap()
-            .find("query")
-            .unwrap_or("/");
-        Ok(Response::with((status::Ok, *query)))
-    }
+fn get_lights_handler(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "OK")))
+}
 
-    fn lights_handler(req: &mut Request) -> IronResult<Response> {
-        // let ref url = req.url;
-        // println!("URL: {:?}", url);
+fn set_lights_handler(req: &mut Request) -> IronResult<Response> {
+    // let ref url = req.url;
+    // println!("URL: {:?}", url);
 
-        let ref params = req.get_ref::<Params>().unwrap();
-        let param = params.find(&["q"]).unwrap();
-        let query = if let &Value::String(ref x) = param {
-            &x
-        } else {
-            ""
-        };
+    let ref params = req.get_ref::<Params>().unwrap();
+    let param = params.find(&["q"]).unwrap();
+    let query = if let &Value::String(ref x) = param {
+        x.to_lowercase()
+    } else {
+        "".to_string()
+    };
 
-        println!("lights {}", query);
+    println!("lights {}", query);
 
-        let (r, g, b) = match query {
-            "off" => named::BLACK,
-            "on" => named::WHITE,
-            "0" => named::BLACK,
-            "10" => named::WHITE,
-            "false" => named::BLACK,
-            "true" => named::WHITE,
-            q => named::from_str(q).unwrap_or(named::WHITE),
-        };
+    let (r, g, b) = match query.as_ref() {
+        "off" => named::BLACK,
+        "on" => named::WHITE,
+        "0" => named::BLACK,
+        "10" => named::WHITE,
+        "false" => named::BLACK,
+        "true" => named::WHITE,
+        q => named::from_str(&q).unwrap_or(named::WHITE),
+    };
 
-        Command::new("python")
-            .args(&["/home/pi/Pimoroni/mote/examples/rgb.py",
-                    &format!("{}", r),
-                    &format!("{}", g),
-                    &format!("{}", b)])
-            .output()
-            .expect("failed to execute process");
+    Command::new("python")
+        .args(&["/home/pi/Pimoroni/mote/examples/rgb.py",
+                &format!("{}", r),
+                &format!("{}", g),
+                &format!("{}", b)])
+        .output()
+        .expect("failed to execute process");
 
-        Ok(Response::with((status::Ok, "OK")))
-    }
+    Ok(Response::with((status::Ok, "OK")))
 }
